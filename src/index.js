44c114451c2788614c5611a61bd6adcaba4c5f4d
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-18 13:40:50 -0400

    Use spread operator instead of our big long if/else

--- src/index.js
+++ src/index.js
@@ -3,3 +3,24 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// // ES6 Spread operator!
+// const citrus = ["Lime", "Lemon", "Orange"];
+//                                              // Spread!
+// const fruit = ["Apple", "Banana", "Coconut", ...citrus];
+
+// console.log(fruit);
+
+// // Spread operator can also be used with objects.
+// const fullName = {
+//   fName: "Jenny",
+//   lName: "Mikac"
+// }
+
+// const user = {
+//   ...fullName,
+//   id: 1,
+//   usrname: "jennyrox1986"
+// }
+
+// console.log(user);
